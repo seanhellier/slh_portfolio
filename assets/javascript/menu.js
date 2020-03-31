a89bbcce9a5fef9ed52514df7503913b723b24c4
Sean Leif Hellier <sean@newtropolis.com>
2020-03-31 13:11:36 -0400

    fixed issue with landing page elements placing and dropdown menu functionality (thank you, Tony Ho! You are a wizard

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,3 +1,13 @@
 $('.navbar-nav>li>a').on('click', function () {
+    if (!this.classList.contains("dropdown-toggle")) {
+        $('.navbar-collapse').collapse('hide');
+    }
+});
+
+$('.navbar-nav>li>div>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('.navbar>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
