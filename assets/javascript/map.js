476296f493852529add7c91128af3c8a5ba15f3b
Sean Leif Hellier <sean@newtropolis.com>
2020-04-03 21:20:07 -0400

    added map to contact page

---
+++ assets/javascript/map.js
@@ -0,0 +1,20 @@
+ // Initialize and add the map
+ function initMap() {
+     // The location of Uluru
+     var nyc = {
+         lat: 40.7128,
+         lng: -74.0060
+     };
+     // The map, centered at Uluru
+     var map = new google.maps.Map(
+         document.getElementById('map'), {
+             zoom: 4,
+             center: nyc,
+             mapTypeId: 'satellite'
+         });
+     // The marker, positioned at Uluru
+     var marker = new google.maps.Marker({
+         position: nyc,
+         map: map
+     });
+ }
